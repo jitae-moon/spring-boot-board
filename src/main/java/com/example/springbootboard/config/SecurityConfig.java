36161e0e29b735dbd2f321e0aeb76af776a6b273
jitae-moon <jitae.moon@icloud.com>
2024-08-24 16:24:37 +0900

    Add default login page

--- src/main/java/com/example/springbootboard/config/SecurityConfig.java
+++ src/main/java/com/example/springbootboard/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springbootboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,9 +10,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
                 .build();
     }
 

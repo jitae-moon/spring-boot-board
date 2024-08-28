d1ca2f82a17b5adb027ea0ea6c4ae7979907d157
jitae-moon <jitae.moon@icloud.com>
2024-08-28 16:08:35 +0900

    Modify repositories to use Querydsl

--- src/main/java/com/example/springbootboard/repository/ArticleRepository.java
+++ src/main/java/com/example/springbootboard/repository/ArticleRepository.java
@@ -1,9 +1,27 @@
 package com.example.springbootboard.repository;
 
 import com.example.springbootboard.domain.Article;
+import com.example.springbootboard.domain.QArticle;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+    }
+
 }
